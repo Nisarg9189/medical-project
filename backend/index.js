e747075f2e24eb15c2286ec6cda61834a7d0f7be
Nisarg Patel <nisargpatel460@gmail.com>
2026-01-25 10:20:40 +0530

    fix

--- backend/index.js
+++ backend/index.js
@@ -44,8 +44,8 @@ const sessionInfo = {
     expires: Date.now() + 7 * 24 * 60 * 60 * 1000,
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    secure: false,
-    sameSite: "lax"
+    secure: true,
+    sameSite: "none"
   },
 };
 
