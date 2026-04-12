f61bda6b5a632575c09138b116ff18f0614a2229
netvincible <netvincible@gmail.com>
2026-04-12 13:00:08 +0530

    switched to local db

--- backend/index.js
+++ backend/index.js
@@ -97,8 +97,8 @@ main().then(() => {
 })
 
 async function main() {
-  // await mongoose.connect('mongodb://127.0.0.1:27017/ruralhospital');
-  await mongoose.connect(dbUrl);
+  await mongoose.connect('mongodb://127.0.0.1:27017/ruralhospital');
+  // await mongoose.connect(dbUrl);
 
   // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
 }
